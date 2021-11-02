cb651b16a7ad9ec5c59b8ca9fe0cae3ec5f14ba4
iliaturin2007 <63121585+iliaturin2007@users.noreply.github.com>
2021-11-02 23:20:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,27 @@
 
 using namespace std;
 
+
+//Класс NamePrice для добавления позиций
 class NamePrice{
+    //Динамические массивы - что бы память расширялась
     string *name;
     int *cost;
+    //Длиная массива(нужна чтобы понять сколько элементов в массиве)
     int length;
     public:
+    //Конструктор
     NamePrice(){
         name = nullptr;
         cost = nullptr;
         length = 0;
     }
+    //Деструктор
     ~NamePrice(){
         delete [] name;
         delete [] cost;
     }
+    //Метод для добавления позиции
     void Add(string Name, int Cost){
         string *out = new string[length+1];
         int *outCost = new int[length+1];
@@ -32,6 +39,7 @@ class NamePrice{
         cost = outCost;
         length++;
     }
+    //Перегруженый оператор () для получения цены по позиции (возвращает ноль, если позиция не существует)
     int operator()(string str){
         for(int i=0; i<length; i++){
             if (str == name[i])
@@ -39,6 +47,7 @@ class NamePrice{
         }
         return 0;
     }
+    //Метод возвращающий названия позиций в скобках через запятую
     string Out(){
         string out;
         out.push_back('(');
@@ -56,15 +65,21 @@ class NamePrice{
 
 
 int main(){
+    //Локализация(часть компиляторов на переваривает русский)
     setlocale(LC_ALL, "Russian");
+    //Объекты класса NamePrice для хранения цветов и типов предметов для покраски
     NamePrice colorTypes, type;
+    //Добавление позиций
     colorTypes.Add("Белый", 1);
     colorTypes.Add("Синий", 2);
     colorTypes.Add("Красный", 3);
     type.Add("Кисть", 3);
     type.Add("Валик", 4);
+    //Переменный размера потолка, цены краски
     int size, countPaint;
+    //Строки принимающие цвет и предмет для покраски
     string color, item;
+    //Куча ввода-вывода(тут вроде всё просто и понятно)
     cout << "Площадь потолка в метрах: ";
     cin >> size;
     cout << "Количество литров краски на 1 квадратный метр: ";
@@ -73,14 +88,16 @@ int main(){
     cin >> color;
     cout << "Чем будете красить: " << type.Out();
     cin >> item;
-    
+    //Перемножение всех данных
     int ans = size*countPaint*colorTypes(color)*type(item);
+    //Если произведение равно нулю - то данные неверны(тк при введении невеной позиции возвращает 0)
     if (ans == 0)
         cout << "Введены неверные данные";
+    //Иначе выводим результат
     else
         cout << "Стоимость покраски составит - " << ans << " рублей";
-
-    cout << colorTypes.Out();
+    //Вывод двух пустых строк(для разделения)
     cout << endl << endl;
+    //Окончание программы(по стандарту c++ функция main должна возвращать 0, ведь в случает ошибки будет возвращено другое значение)
     return 0;
 }
