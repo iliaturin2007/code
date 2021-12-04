89365404ffe9ecf82fd433c9a48742e952b54237
iliaturin2007 <63121585+iliaturin2007@users.noreply.github.com>
2021-12-05 00:14:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,219 +1,134 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
-//Добавляем пространство имён std
 using namespace std;
 
 
-//Класс NamePrice для добавления позиций
-class NamePrice{
-    //Динамические массивы - что бы память расширялась
-    string *name;
-    int *cost;
-    //Длиная массива(нужна чтобы понять сколько элементов в массиве)
-    int length;
-public:
-    //Конструктор
-    NamePrice(){
-        name = nullptr;
-        cost = nullptr;
-        length = 0;
-    }
-    //Конструктор копирования
-    NamePrice(NamePrice &obj){
-        this->length = obj.length;
-        this->name = new string[length];
-        this->cost = new int[length];
-        for(int i=0; i<length; i++){
-            this->cost[i] = obj.cost[i];
-            this->name[i] = obj.name[i];
-        }
-    }
-    //Перегрузка оператора =(для нормальной передачи в функцию)
-    void operator=(NamePrice &obj){
-        this->length = obj.length;
-        this->name = new string[length];
-        this->cost = new int[length];
-        for(int i=0; i<length; i++){
-            this->cost[i] = obj.cost[i];
-            this->name[i] = obj.name[i];
-        }
-    }
-    //Деструктор
-    ~NamePrice(){
-        delete [] name;
-        delete [] cost;
-    }
-    //Метод для добавления позиции
-    void Add(string Name, int Cost){
-        //создаём массив имён который на один элемент больше старого
-        string *out = new string[length+1];
-        //создаём массив значений который на один элемент больше старого
-        int *outCost = new int[length+1];
-        //переносим элементы из основного массива в только что созданные
-        for (int i = 0; i<length; i++){
-            out[i] = name[i];
-            outCost[i] = cost[i];
-        }
-        //Удаляет массивы
-        delete [] name;
-        delete [] cost;
-        //Добавляем аргументы в массив
-        out[length] = Name;
-        outCost[length] = Cost;
-        //Присваевам новый расширенный массив к старому указателю
-        name = out;
-        cost = outCost;
-        //Меняем длину
-        length++;
-    }
-    //Перегруженый оператор () для получения цены по позиции (возвращает ноль, если позиция не существует)
-    int operator()(string str){
-        for(int i=0; i<length; i++){
-            if (str == name[i])
-                return cost[i];
-        }
-        return 0;
-    }
-    //Вывод всех имём отсортированного по значениям в порядке убывания
-    string* AllNames(){
-        string *out = new string[length];
-        //Перенос массива
-        for (int i = 0; i<length; i++){
-            out[i] = name[i];
-        }
-        //Сортировка
-        for (int i=0; i<length; i++){
-            for (int j=i; j<length-1; j++){
-                if (operator()(out[i])<operator()(out[i+1])){
-                    swap(out[j], out[j+1]);
-                }
-            }
-        }
-        return out;
-    }
-    int l(){
-        return length;
-    }
+struct paint_item {
+    int type;
+    string color; 
+    int volume;
+    double price;
 };
 
-class Painting{
-    private:
-        int budget, size;
-        int paintConsuption, volume, container, countLayers, spent, countCans;
-        string colorname;
-        NamePrice paints;
-    public:
-        //Конструктор
-        Painting(){
-            budget = 0;
-            size = 0;
-        }
-        //Конструктор для аргументов
-        Painting(int budget, int size, NamePrice paints, int paintConsuption){
-            this->budget = budget;
-            this->size = size;
-            this->paints = paints;
-            this->paintConsuption = paintConsuption;
-        }
-        //Функция оптимального подсчёта покраски
-        void OptimalPainting(){
-            string *color = paints.AllNames();
-            int cost;
-            countLayers = -1;
-            for (int i=0; i<paints.l(); i++){
-                volume = size*paintConsuption;
-                cost = paints(color[i]);
-                if (cost*volume <= budget){
-                    countLayers = ((budget/(cost*volume))<=5)?(budget/(cost*volume)):(5);
-                    spent = cost*volume*countLayers;
-                    colorname = color[i];
-                    container = OptimalVolume(volume);
-                    countCans = volume/container;
-                    if (volume%container != 0)
-                        countCans++;
-                    break;
-                }
-            }
-            delete [] color;
-        }
-        //Высчитывание оптимальной ёмкости
-        int OptimalVolume(const int volume){
-            int th = (3-volume%3)%3, f = (5-volume%5)%5, t=(10-volume%10)%10;
-            if (t<=f&&t<=th)
-                return 10;
-            else if (f<=t&&f<=th)
-                return 5;
-            else
-                return 3;
-        }
-        //Перезрузка вывода
-        friend ostream& operator<<(ostream &out, Painting obj);
-};
-//Вывод
-ostream& operator<<(ostream &out, Painting obj){
-    if (obj.countLayers>0){
-        out << endl << endl << "Красить " << obj.colorname << " краской в "  << obj.countLayers;
-        if (obj.countLayers == 1)
-            out << " слой";
-        else if (obj.countLayers == 5)
-            out << " слоёв";
-        else
-            out << " слоя";
-        out << endl << "Нужно закупить " << obj.countCans;
-        if ((obj.countCans>4&&obj.countCans<21)||obj.countCans%10==0||(obj.countCans%10>4&&obj.countCans%10<10))
-            out << " банок";
-        else if(obj.countCans==1)
-            out << " банку";
-        else
-            out << " банки";
-        out << " по " << obj.container;
-        if (obj.container == 3)
-            out << " литра" << endl;
-        else
-            out << " литров" << endl;
-        int extra = obj.countCans*obj.container-obj.volume;
-        if (extra!=0){
-            out << "Останутся лишними " << extra;
-            if (extra == 1)
-                out << " литр";
-            else if (obj.countLayers >= 5)
-                out << " литров";
-            else
-                out << " литра";
-            cout << " краски" << endl;
-        }
-        out << "Будет потрачено " << obj.spent << " рублей";
-    }
-    else
-        out << "Невозможно покрасить такую площать при таком маленьком бюджетом!!!";
-    return out;
+double coloring_cost(double square, int typeCost, int colorCost, int layerNumber){
+    return square*(typeCost+1)*colorCost*layerNumber;
 }
 
-int main(){
-    //Локализация(часть компиляторов на переваривает русский)
-    setlocale(LC_ALL, "Russian");
-    //Объект с красками
-    NamePrice paints;
-    //Добавление краски
-    paints.Add("красной эмалевой", 2);
-    paints.Add("синей клеевой", 15);
-    paints.Add("белой водоэмульсионной", 5);
-    //Расход краски на метр квадратный для обного слоя
-    const int paintConsuption = 1;
-    int budget, size;
-    //Ввод
-    cout << "На какую сумму вы хотите покрасить: ";
-    cin >> budget;
-    cout << "Какой метраж вы хотите покрасить: ";
-    cin >> size;
-    //Создание класса покраски
-    Painting p(budget, size, paints, paintConsuption); 
-    //Вызов метода для оптимального подсчёта
-    p.OptimalPainting();
-    //Вывод
-    cout << p;
-    cout << endl << endl;
-    //Конец программы
+int package_optional(double volumePackage, double volume){
+    return int(volume/volumePackage)+(volume-(volumePackage*int(volume/volumePackage))==0?0:1);
+}
+void budget_options(double square, int budget, vector<paint_item> paints){
+    int layers_number=2;
+    paint_item current;
+    bool colorChoice = false;
+    for (int i=0; i<paints.size(); i++){
+        if (package_optional(paints[i].volume, square*2)*paints[i].price*(paints[i].type+1)<=budget){
+            while(package_optional(paints[i].volume, square*(layers_number+1))*paints[i].price*(paints[i].type+1) < budget)
+                layers_number++;
+            current = paints[i];
+            colorChoice = true;
+            break;
+        }
+    }
+    if (colorChoice){
+        if (layers_number > 7)
+            layers_number = 7;
+        cout << "Стоимость краски в рублях: " << package_optional(current.volume, current.volume*layers_number)*current.price*(current.type+1) <<
+        endl << "Цвет краски - " << current.color << endl << "Количество банок " << package_optional(current.volume, square*layers_number) << endl
+        << "Количество слоёв для покраски: " << layers_number << endl << "Сорт краски: " << (current.volume?"высший":"стандартный") << endl;
+    }
+    else
+        cout << "Невозможно покрасить - слишком низкий бюджет" << endl;
+}
+
+void MainMenu(int com, vector<paint_item> paints){
+    if (com == 1){
+        cout << endl << endl;
+        cout << "Все виды красок: " << endl << endl;
+        for (int i=0; i<paints.size(); i++){
+            cout << "Цвет - " << paints[i].color << ", сорт - " << (paints[i].type?"высший":"стандартны")
+            << ", цена - " << paints[i].price << ", объём банки - " << paints[i].volume << endl;
+        }
+        cout << endl << endl;
+    }
+    if (com == 2){
+        cout << endl << endl;
+        paint_item it;
+        double square;
+        int layerNumber, paintsChoice;
+        cout << "Введите площадь: ";
+        cin >> square;
+        for (int i=0; i<paints.size(); i++){
+            cout << i+1 << ") Цвет " << paints[i].color << " Цена - " << (paints[i].type+1)*paints[i].price << " ";
+        }
+        cout << endl << "Введите номер краски: ";
+        cin >> paintsChoice;
+        it = paints[paintsChoice-1];
+        cout << "Введите количество слоёв: ";
+        cin >> layerNumber;
+        cout << endl << "Стоимость покраски составит - " << coloring_cost(square, it.type, it.price, layerNumber);
+        cout << endl << endl << endl;
+    }
+    if (com == 3){
+        double width, height, square=0;
+        int budget;
+        string command;
+        cout << "Введите свой бюджет: ";
+        cin >> budget;
+        cout << "Введите + чтобы добавить стену и - чтобы добавить выподающий участок. Для продолжения - 0" << endl;
+        cin >> command;
+        while (command != "0"){
+            if (command == "+"){
+                cout << "Ширина: ";
+                cin >> width;
+                cout << "Высота: ";
+                cin >> height;
+                square += width*height;
+            }
+            else if (command == "-"){
+                cout << "Ширина: ";
+                cin >> width;
+                cout << "Высота: ";
+                cin >> height;
+                square -= width*height;
+                if (square < 0)
+                    square = 0;
+            }
+            else{
+                cout << "Такой команды не существует" << endl;
+            }
+            cin >> command;
+        }
+        cout << endl << endl;
+        budget_options(square, budget, paints);
+        cout << endl << endl;
+    }
+    //Справка
+    if (com == 4){
+        cout << endl << endl;
+        cout << "Заполните справку";
+        cout << endl << endl << endl;
+    }
+}
+
+int main()
+{
+    vector<paint_item> paints;
+    //Важно добавлять цвета в нужном порядке - сначало дорогие потом дешовые
+    paints.push_back({1, "Красный", 5, 50});
+    paints.push_back({1, "Синий", 5, 20});
+    paints.push_back({1, "Чёрный", 5, 10});
+    paints.push_back({0, "Белый", 5, 10});
+    int command;
+    cout << "Введите режим(1 - Справочник красок, 2 - Поверхность и стоимость, 3 - Нормы и ограничения, 4 - Справка, 5 - Выход): ";
+    cin >> command;
+    while (command != 5){
+        MainMenu(command, paints);
+        cout << "Введите режим(1 - Справочник красок, 2 - Поверхность и стоимость, 3 - Нормы и ограничения, 4 - Справка, 5 - Выход): ";
+        cin >> command;
+    }
+    int width, height;
     return 0;
 }
